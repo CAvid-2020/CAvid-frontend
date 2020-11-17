092d04ff99912d5e3661f355998f5a833cb3c404
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-11-17 21:01:50 +0200

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { Button, Section } from "../../components";
+import React, { useEffect, useState } from "react";
+import { Table, Section, Button } from "../../components";
+import * as S from "./Home.style";
+
+function sendData(radio, student, e) {
+  e.preventDefault();
+  console.log(radio);
+  fetch(`http://localhost:8080/attendance`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      student_id: student.id,
+      attendance: radio,
+      date: new Date(),
+    }),
+  });
+}
 
 function Home() {
+  const [radio, setRadio] = useState();
+  const [students, setStudents] = useState();
+  console.log(radio);
+  console.log(students);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/students`)
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data);
+      });
+  }, []);
   return (
     <>
       <Section>
-        <Button color="primary" handleClick={() => alert("Hi")}>
-          Check React out
-        </Button>
+        <form onSubmit={(e) => sendData(radio, students, e)}>
+          <Table
+            students={students}
+            callback={(e) => console.log(e.target.value)}
+          />
+          <S.ButtonBox>
+            <Button color="primary">Submit</Button>
+          </S.ButtonBox>
+        </form>
       </Section>
     </>
   );
---
+++ src/pages/Home/Home.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ButtonBox = styled.div`
+  margin-top: 1em;
+  text-align: center;
+`;
