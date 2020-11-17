e3b4012ca5a4137b5c980198991df4e63e9f5d71
Marius <grausas@gmail.com>
2020-11-17 18:36:59 +0200

    added style for navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import * as S from "./Header.style";
-import logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <S.Header>
-      <Link to="/">
-        <S.Logo src={logo} alt="logo" />
-      </Link>
-
       <S.Actions>
         <S.StyledLink to="/">Home</S.StyledLink>
         <S.StyledLink to="/about">About</S.StyledLink>
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -3,16 +3,11 @@ import { Link } from "react-router-dom";
 
 export const Header = styled.header`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   box-shadow: 0 1px 1px 2px goldenrod;
   padding: 1em;
-  background-color: rgb(147, 193, 31);
-`;
-
-export const Logo = styled.img`
-  max-height: 45px;
-  max-width: 100%;
+  background-color: ${(props) => props.theme.darkGreen};
 `;
 
 export const Actions = styled.nav``;
@@ -20,9 +15,10 @@ export const Actions = styled.nav``;
 export const StyledLink = styled(Link)`
   margin-left: 5px;
   text-decoration: none;
+  color: #fff;
   &:hover {
     text-decoration: underline;
-    transition: 1s;
+    transition: 0.5s;
     color: goldenrod;
   }
   &:not(:last-child) {
