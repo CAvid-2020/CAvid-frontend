03c4c7e8860170bfa48541ad4f648c420189cea0
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-11-19 10:01:55 +0200

    replace pre-post data validation with backend

--- src/pages/RegisterAttendance/RegisterAttendance.jsx
+++ src/pages/RegisterAttendance/RegisterAttendance.jsx
@@ -11,8 +11,6 @@ function sendData(studentid, history) {
     },
     body: JSON.stringify({
       student_id: studentid,
-      attendance: "true",
-      date: new Date(),
     }),
   })
     .then((res) => res.json())
@@ -25,7 +23,7 @@ function sendData(studentid, history) {
 function RegisterAttendance() {
   const [students, setStudents] = useState();
   const [studentid, setStudentsId] = useState();
-  const [time, setTime] = useState();
+  const time = 19;
   const history = useHistory();
 
   function validateData() {
@@ -46,11 +44,6 @@ function RegisterAttendance() {
       .then((res) => res.json())
       .then((data) => {
         setStudents(data);
-      });
-    fetch(`http://localhost:8080/date`)
-      .then((res) => res.json())
-      .then((data) => {
-        setTime(data);
       });
   }, []);
 
